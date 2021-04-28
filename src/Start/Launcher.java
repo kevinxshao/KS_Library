6c0030a69b82faa8a4c422ee16a4bf1704f77087
kevinxshao <kevinxshao@gmail.com>
2021-04-27 22:15:05 -0400

    Start of custom ArrayList class.

--- src/Start/Launcher.java
+++ src/Start/Launcher.java
@@ -1,5 +1,7 @@
 package Start;
 
 public class Launcher {
-
+	public static void main(String[] args) {
+		
+	}
 }
---
+++ src/data_structures/KS_ArrayList.java
@@ -0,0 +1,12 @@
+package data_structures;
+
+public class KS_ArrayList {
+	private int numOfItems = 0;
+	
+	public int size() {
+		
+		return numOfItems;
+	}
+	
+
+}

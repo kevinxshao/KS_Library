a1f12f36e062423f9e1a4aae6927d4c1aeaee4cd
kevinxshao <kevinxshao@gmail.com>
2021-04-28 22:11:27 -0400

    created prototype add, get and size method implementations

--- src/Start/Launcher.java
+++ src/Start/Launcher.java
@@ -1,7 +1,14 @@
 package Start;
+import data_structures.KS_ArrayList;
 
 public class Launcher {
 	public static void main(String[] args) {
-		
+		KS_ArrayList ks = new KS_ArrayList();
+		ks.add(1);
+		ks.add(2);
+		ks.add(3);
+		for(int i =0; i < ks.size(); i++) {
+			System.out.println(ks.get(i));
+		}
 	}
 }
--- src/data_structures/KS_ArrayList.java
+++ src/data_structures/KS_ArrayList.java
@@ -1,12 +1,27 @@
 package data_structures;
 
 public class KS_ArrayList {
-	private int numOfItems = 0;
-	
+	private Object arr[];
+	private int numOfItems;
+	private int capacity;
+
+	public KS_ArrayList() {
+		capacity =10;
+		numOfItems = 0;
+		arr = new Object[capacity];
+	}
+	public void add(Object element) {
+		arr[numOfItems] = element;
+		numOfItems++;
+	}
+	public Object get(int index) {
+		if(index > numOfItems-1) {
+			throw new java.lang.IndexOutOfBoundsException();
+		}
+		return arr[index];
+	}
 	public int size() {
-		
 		return numOfItems;
 	}
-	
 
 }

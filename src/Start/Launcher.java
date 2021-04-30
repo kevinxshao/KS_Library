e0605a2705f1a6a5f8d74549a89f89c20b851988
kevinxshao <kevinxshao@gmail.com>
2021-04-29 23:51:05 -0400

    finished base class for ArrayList implemented base methods

--- src/Start/Launcher.java
+++ src/Start/Launcher.java
@@ -3,12 +3,12 @@ import data_structures.KS_ArrayList;
 
 public class Launcher {
 	public static void main(String[] args) {
-		KS_ArrayList ks = new KS_ArrayList();
-		ks.add(1);
-		ks.add(2);
-		ks.add(3);
-		for(int i =0; i < ks.size(); i++) {
-			System.out.println(ks.get(i));
-		}
+		KS_ArrayList<Integer> ks = new KS_ArrayList<>();
+		System.out.println(ks.currentCapacity());
+		for(int i =1; i <=16; i++) ks.add(i);
+		ks.add(17);
+		System.out.println(ks.currentCapacity());
+		for(int i=0; i <ks.size();i++)System.out.println(ks.get(i));
+		
 	}
 }
--- src/data_structures/KS_ArrayList.java
+++ src/data_structures/KS_ArrayList.java
@@ -1,6 +1,6 @@
 package data_structures;
 
-public class KS_ArrayList {
+public class KS_ArrayList<T> {
 	private Object arr[];
 	private int numOfItems;
 	private int capacity;
@@ -10,7 +10,23 @@ public class KS_ArrayList {
 		numOfItems = 0;
 		arr = new Object[capacity];
 	}
-	public void add(Object element) {
+	public KS_ArrayList(int c) {
+		capacity = c;
+		numOfItems = 0;
+		arr = new Object[capacity];
+	}
+	public void add(T element) {
+		if(numOfItems == capacity) {
+			Object copyarr[] = new Object[numOfItems];
+			for(int i = 0; i < numOfItems; i++) {
+				copyarr[i] = arr[i];
+			}
+			capacity = capacity*3/2+1;
+			arr = new Object[capacity];
+			for(int i =0; i < numOfItems; i++) {
+				arr[i] = copyarr[i];
+			}
+		}
 		arr[numOfItems] = element;
 		numOfItems++;
 	}
@@ -18,10 +34,24 @@ public class KS_ArrayList {
 		if(index > numOfItems-1) {
 			throw new java.lang.IndexOutOfBoundsException();
 		}
-		return arr[index];
+		return (T) arr[index];
 	}
 	public int size() {
 		return numOfItems;
 	}
+	
+	public boolean isEmpty() {
+		if(numOfItems ==0) {
+			return true;
+		}
+		else {
+			return false;
+		}
+	}
+	
+	public int currentCapacity() {
+		return capacity;
+	}
+	
 
 }
